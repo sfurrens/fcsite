e2b26306dd0f5390853afff9f643597d8cfbde79
dimensioon <dimensioon@ailsac.com>
2018-05-27 13:14:49 +0200

    implement db conn

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+
+// Report all PHP errors (see changelog)
+error_reporting(E_ALL);
+
+// Report all PHP errors
+error_reporting(-1);
+
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+include('sql/conn.php');
+
+var_dump(get_news());
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     
--- sql/conn.php
+++ sql/conn.php
@@ -1,13 +1,35 @@
 <?php
 
-    $_HOST_             = "sql313.hostfree.pw";
-    $_DBUSER_           = "epree_22138974";
-    $_DBPASS_           = "pingpong";
-    $_DATABASE_         = "epree_22138974";
+$_HOST_             = "sql313.hostfree.pw";
+$_DBUSER_           = "epree_22138974";
+$_DBPASS_           = "pingpong";
+$_DATABASE_         = "epree_22138974_fc";
 
 
-    $_DB_NEWS_           = "news";
+$_DB_NEWS_           = "news";
 
-    $mysqli = new mysqli($_HOST_, $_DBUSER_, $_DBPASS_, $_DATABASE_)
+$mysqli = new mysqli($_HOST_, $_DBUSER_, $_DBPASS_, $_DATABASE_);
+
+function get_news() {
+    global $mysqli, $_DB_NEWS_;
     
+    $news = array();
+    
+    // get all data from news table
+    $res = $mysqli->query("select * from `".$_DB_NEWS_."`;");
+    //$res = $mysqli->query("show tables;");
+    
+    // parse all rows
+    while ($r = $res->fetch_assoc()) {
+        $new = array(
+            "title" => $r['title'],
+            "description" => $r['description'],
+            "date" => $r['date'],
+            "imagePath" => $r['imagePath']
+        );
+        array_push($news, $new);
+    }
+    
+    return $news;
+}
 ?>
