6906f51e0c859651fc432fcab17ae965584f5e27
dimensioon <dimensioon@ailsac.com>
2018-05-27 15:13:20 +0200

    name change

---
+++ nav.php
@@ -0,0 +1,30 @@
+<section id="submenu-container">
+    <a id="mobile-menu" href="index.html#">Sub Menu</a>
+    <nav id="submenu">
+        <ul>
+            <li>
+                <a href="index.html">Home</a>
+            </li>
+            <li>
+                <a href="club.html">Our Club</a>
+            </li>
+            <li>
+                <a href="galleries.html">Gallery</a>
+            </li>
+            <li>
+                <a href="contact.html">Contact</a>
+            </li>
+        </ul>
+    </nav>
+    <!-- LOGIN MENU (not implemented yet) -->
+    <!-- nav id="submenu-login">
+        <ul>
+            <li>
+                <a href="index.html#">Register</a>
+            </li>
+            <li>
+                <a href="index.html#">Login</a>
+            </li>
+        </ul>
+    </nav -->
+</section>
