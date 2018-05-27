4f6ec7f1e965a8bb9b3c1ddc45b5ff5b88ad5f30
dimensioon <dimensioon@ailsac.com>
2018-05-27 12:20:35 +0200

    init

---
+++ sql/conn.php
@@ -0,0 +1,13 @@
+<?php
+
+    $_HOST_             = "sql313.hostfree.pw";
+    $_DBUSER_           = "epree_22138974";
+    $_DBPASS_           = "pingpong";
+    $_DATABASE_         = "epree_22138974";
+
+
+    $_DB_NEWS_           = "news";
+
+    $mysqli = new mysqli($_HOST_, $_DBUSER_, $_DBPASS_, $_DATABASE_)
+    
+?>
