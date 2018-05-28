542e4e8fb9720332c528e3017e726ad205fed872
dimensioon <dimensioon@ailsac.com>
2018-05-28 18:18:52 +0200

    seperated to file from design file

---
+++ footer.php
@@ -0,0 +1,94 @@
+<footer id="footer">
+    <div id="footer-widgets">
+        <!-- WIDGET 1 -->
+        <div class="footer-widget first-clmn">
+            <h5>Recent Posts</h5>
+            <ul>
+                <li>
+                    <a href="blog.html">Suspendisse ipsum urna</a>
+                </li>
+                <li>
+                    <a href="blog.html">Pellentesque eget por</a>
+                </li>
+                <li>
+                    <a href="blog.html">Aliquam sit amet urne</a>
+                </li>
+                <li>
+                    <a href="blog.html">Ornare dapibus ornare et</a>
+                </li>
+                <li>
+                    <a href="blog.html">Donec et odio et amet</a>
+                </li>
+            </ul>
+        </div>
+        <!-- WIDGET 2 -->
+        <div class="footer-widget second-clmn">
+            <h5>Categories</h5>
+            <ul>
+                <li>
+                    <a href="single-post.html">Lorem Ipsum Dolor eget por</a>
+                </li>
+                <li>
+                    <a href="single-post.html">Nibh Mauris lucem id sagit</a>
+                </li>
+                <li>
+                    <a href="single-post.html">Donec in lorem mauris</a>
+                </li>
+                <li>
+                    <a href="single-post.html">Auctor Mauris Dolor urna loe</a>
+                </li>
+                <li>
+                    <a href="single-post.html">Mauris id posuere</a>
+                </li>
+            </ul>
+        </div>
+        <!-- WIDGET 3 -->
+        <div class="footer-widget third-clmn">
+            <h5>Flickr Feed</h5>
+            <!-- FLICKR WIDGET -->
+            <ul id="cbox" class="flickr-box"></ul>
+            <!-- END -->
+        </div>
+    </div>
+    <div class="clear"></div>
+    <!-- COPYRIGHT -->
+    <div class="credits">Copyright Egemenerd</div>
+    <!-- SOCIAL ICONS -->
+    <ul class="social-icons">
+        <li>
+            <a href="index.html#">
+                <img class="social-icon" src="images/social-icons/facebook.png" alt="Facebook" />
+            </a>
+        </li>
+        <li>
+            <a href="index.html#">
+                <img class="social-icon" src="images/social-icons/twitter.png" alt="Twitter" />
+            </a>
+        </li>
+        <li>
+            <a href="index.html#">
+                <img class="social-icon" src="images/social-icons/flickr.png" alt="Flickr" />
+            </a>
+        </li>
+        <li>
+            <a href="index.html#">
+                <img class="social-icon" src="images/social-icons/linkedin.png" alt="Linked in" />
+            </a>
+        </li>
+        <li>
+            <a href="index.html#">
+                <img class="social-icon" src="images/social-icons/google+.png" alt="Google+" />
+            </a>
+        </li>
+        <li>
+            <a href="index.html#">
+                <img class="social-icon" src="images/social-icons/vimeo.png" alt="Vimeo" />
+            </a>
+        </li>
+        <li>
+            <a href="index.html#">
+                <img class="social-icon" src="images/social-icons/youtube.png" alt="YouTube" />
+            </a>
+        </li>
+    </ul>
+</footer>
